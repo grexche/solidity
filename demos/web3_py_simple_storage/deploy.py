439e501f24dae9d7ae4e4de937aecfcdf681c831
陈荣 <chenrong@custouch.com>
2022-06-09 22:23:16 +0800

    compile in python

---
+++ demos/web3_py_simple_storage/deploy.py
@@ -0,0 +1,30 @@
+from solcx import compile_standard
+import json
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+    print(simple_storage_file)
+
+compile_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.8.14",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compile_sol, file)
+
+# get bytecode
+bytecode = compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
