6c1a047faed253e3b5c3beb47de6bede84668c8a
陈荣 <chenrong@custouch.com>
2022-06-11 00:58:49 +0800

    build and sign a transaction

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,9 +1,11 @@
 from solcx import compile_standard
 import json
+from web3 import Web3
+import os
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+
 
 compile_sol = compile_standard(
     {
@@ -28,3 +30,31 @@ bytecode = compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]
 
 # get abi
 abi = compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x00a729A51b5b0ca68aeAa32B58711c3546c9d3Eb"
+private_key = "0xe3db09f74867d66902094237108556aeafcc7b439ef0787fe08a82ad1bd0950c"
+
+# create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# get latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
+
+
+# 1. build a transaction
+# 2. sign a transaction
+# 3. send a transaction
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+print(private_key)
+# signed_txn=w3.eth.account.sign_transaction(transaction,private_key=private_key)
